ff39a21fed637a1090c662ad4792565385cce72e
lucas <lucasportellaagu@gmail.com>
2021-08-18 12:23:51 -0300

    req14

--- challenges/desafio13.js
+++ challenges/desafio13.js
@@ -4,3 +4,4 @@ db.trips.aggregate([
   { $group: { _id: null, media: { $avg: { $subtract: ["$stopTime", "$startTime"] } } } },
   { $project: { _id: 0, duracaoMediaEmMinutos: { $ceil: { $divide: ["$media", 1000 * 60] } } } },
 ]);
+// https://stackoverflow.com/questions/38039705/convert-iso-date-to-yyyy-mm-dd-format
